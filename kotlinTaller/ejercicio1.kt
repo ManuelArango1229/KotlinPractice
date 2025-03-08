fdd9cd1c3221a1950f384d170e6a063a2afdac48
ManuelArango1229 <juan12292004@outlook.com>
2025-03-08 15:20:00 -0500

    ADD: implementation exercise 1

---
+++ kotlinTaller/ejercicio1.kt
@@ -0,0 +1,14 @@
+/*
+ *Autor: Juan Manuel Arango Rodas
+ *Cod Estd: 2259571
+ *Fecha: 2025-03-08
+ *Descripción: Solución ejercicio 1
+ */
+
+fun main() {
+    println("Ingrese su nombre: ")
+    var name: String = readLine()!!
+    println("Ingrese su edad: ")
+    var age: Int = readLine()!!.toInt()
+    println("Hola, mi nombre es $name y tengo $age años.")
+}
