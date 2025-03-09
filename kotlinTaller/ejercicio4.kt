1ea3ad098352a32e59276abc0f99fa3654164113
ManuelArango1229 <juan12292004@outlook.com>
2025-03-08 21:32:31 -0500

    ADD: implementation exercise 4

---
+++ kotlinTaller/ejercicio4.kt
@@ -0,0 +1,13 @@
+/*
+ *Autor: Juan Manuel Arango Rodas
+ *Cod Estd: 2259571
+ *Fecha: 2025-03-08
+ *Descripción: Solución ejercicio 4
+ */
+fun main(args: Array<String>) {
+    val array = arrayOf(10, 20, 30, 40, 50)
+
+    for (x in array) {
+        println(x)
+    }
+}
