846e719e8addb9c1e0f36ff8a1497e3e631f7e1c
ManuelArango1229 <juan12292004@outlook.com>
2025-03-08 20:40:23 -0500

    ADD: implementation exersice 3

---
+++ kotlinTaller/ejercicio3.kt
@@ -0,0 +1,12 @@
+/*
+ *Autor: Juan Manuel Arango Rodas
+ *Cod Estd: 2259571
+ *Fecha: 2025-03-08
+ *Descripción: Solución ejercicio 3
+ */
+
+fun saludar(nombre: String): String = "¡Hola, $nombre!"
+
+fun main(args: Array<String>) {
+    println(saludar("Juan"))
+}
