9b298ba3a029384ea2a6193b1165b55f6e39fc3c
ManuelArango1229 <juan12292004@outlook.com>
2025-03-09 20:04:05 -0500

    ADD: implementation exercise 11

---
+++ kotlinTaller/ejercicio11.kt
@@ -0,0 +1,22 @@
+/*
+ *Autor: Juan Manuel Arango Rodas
+ *Cod Estd: 2259571
+ *Fecha: 2025-03-08
+ *Descripción: Solución ejercicio 3
+*/
+
+fun main(args: Array<String>) {
+    val morningNotification: Int = 51
+    val eveningNotification: Int = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(notification: Int) {
+    when (notification) {
+        in 1..99 -> println("You have $notification notifications")
+        in 101..150 -> println("You phone is blowing up! you have  99+ notifications")
+        else -> println("Unknown notification")
+    }
+}
